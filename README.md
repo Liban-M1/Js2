41a82fc3169233dd03b7eb60ba25942b7013949d
Liban-M1 <114482781+Liban-M1@users.noreply.github.com>
2023-10-24 19:14:59 +0200

    24.oct
    Advanced Styling + Core Functionalities + Features

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# Js2
-JavaScript 2 CA
+# Social Media Front-End Client
+
+## Brief
+
+You have been tasked with producing a new JavaScript front-end client for an existing social media platform. The client application must be attractive and responsive, but you have an otherwise blank slate in terms of theme design language.
+
+## Project Overview
+
+Using the provided API and API documentation, create a functioning user interface that allows for viewing, posting, editing, and deleting social media content.
+
+Social API routes require authorization via JWT (JSON Web Tokens). You will need to register an account and login to access your token.
+
+To complete the required features, you will also need to make use of GET, POST, PUT, and DELETE HTTP methods.
+
+Using localStorage is highly recommended, especially for storing JWT tokens.
+
+A finished project fulfills the requirements below with an easy-to-use and error-free user interface.
+
+## Required Features
+
+The following user stories are required for a passing submission:
+
+1. User with @noroff.no or @stud.noroff.no email can register a profile.
+2. Registered user can log in.
+3. User can view a post content feed.
+4. User can filter the post content feed.
+5. User can search the post content feed.
+6. User can view a post content item by ID.
+7. User can create a post content item.
+8. User can update a post content item.
+9. User can delete a post content item.
+
+## Getting Started
+
+To run this project, follow these steps:
+
+1. Clone this repository to your local machine.
+2. Install any necessary dependencies using `npm install`.
+3. Start the application using `npm start`.
+
+## Usage
+
+- Register a new profile using a valid @noroff.no or @stud.noroff.no email.
+- Log in to access your account.
+- View the post content feed, filter, and search for posts.
+- Click on a post to view it by ID.
+- Create new posts with a title, body, and optional media.
+- Edit existing posts by clicking the "Edit" button.
+- Delete posts by clicking the "Delete" button.
+
+## Contributing
+
+If you'd like to contribute to this project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or fix.
+3. Make your changes and commit them.
+4. Push to your fork and submit a pull request.
+
+## License
+
+This project is not licensed
--- js/postFeed.js
+++ js/postFeed.js
@@ -2,8 +2,8 @@ import config from "./config.js";
 const API_BASE = config.API_BASE;
 const POST_POSTS = config.POST_POSTS;
 const GET_POSTS = config.GET_POSTS;
-
-// POST POSTS
+const PUT_POSTS = config.PUT_POSTS;
+const DELETE_POSTS = config.DELETE_POSTS;
 
 document.addEventListener("DOMContentLoaded", function () {
   const accessToken = localStorage.getItem("accessToken");
@@ -82,68 +82,257 @@ document.addEventListener("DOMContentLoaded", function () {
       return false;
     }
   }
-});
 
-// GET POSTS
-
-document.addEventListener("DOMContentLoaded", async function () {
-    const postFeedElement = document.getElementById("postFeed");
+  async function editPost(postId) {
     const accessToken = localStorage.getItem("accessToken");
-    const loggedInUserEmail = localStorage.getItem("email");
 
     if (!accessToken) {
-        alert("Please log in to view posts.");
-        return;
+      console.error("Access token not found in localStorage");
+      return;
+    }
+
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    };
+
+    const editedElement = document.getElementById(`post-${postId}`);
+    const titleElement = editedElement.querySelector(".card-title");
+    const descriptionElement = editedElement.querySelector(".card-description");
+
+    const newTitle = prompt("Edit Title:", titleElement.textContent);
+    const newBody = prompt("Edit Body:", descriptionElement.textContent);
+
+    if (newTitle === null || newBody === null) {
+      return;
     }
 
     try {
-        const response = await fetch(`${GET_POSTS}`, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
+      const response = await fetch(`${PUT_POSTS}${postId}`, {
+        method: "PUT",
+        headers,
+        body: JSON.stringify({
+          title: newTitle,
+          body: newBody,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to edit post");
+      }
+
+      titleElement.textContent = newTitle;
+      descriptionElement.textContent = newBody;
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while editing the post.");
+    }
+  }
+
+  async function deletePost(postId) {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (!accessToken) {
+      console.error("Access token not found in localStorage");
+      return;
+    }
+
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const confirmation = confirm("Are you sure you want to delete this post?");
+
+    if (confirmation) {
+      try {
+        const response = await fetch(`${DELETE_POSTS}${postId}`, {
+          method: "DELETE",
+          headers,
         });
 
         if (!response.ok) {
-            throw new Error("Failed to fetch posts");
+          throw new Error("Failed to delete post");
         }
 
-        const posts = await response.json();
-        const postsWithMedia = posts.filter(post => post.media);
-
-        if (postFeedElement) {
-            if (postsWithMedia.length === 0) {
-                postFeedElement.innerHTML = "<p>No posts with media available.</p>";
-            } else {
-                postFeedElement.innerHTML = postsWithMedia
-                    .map(
-                        (post) => `
-                        <div class="card">
-                            <img src="${post.media}">
-                            <div class="card-content">
-                                <div class="author-info">
-                                    <div class="profile-picture">
-                                        <img src="${post.author.avatar || "/assets/user_avt.png"}" alt="">
-                                    </div>
-                                    <p class="card-user">${post.author.name}</p>
-                                </div>
-                                <h3 class="card-title">${post.title}</h3>
-                                <p class="card-description">${post.body || ""}</p>
-                                <a class="card-link" href="#">Read More</a>
-                                <a class="card-link" href="#">Share</a>
-                                ${post.author.email === loggedInUserEmail ? `<a class="card-link deleteBtn" href="#">Delete</a>` : ''}
-                            </div>
-                        </div>
-                    `
-                    )
-                    .join("");
-            }
+        const deletedElement = document.getElementById(`post-${postId}`);
+        if (deletedElement) {
+          deletedElement.remove();
         }
-    } catch (error) {
+      } catch (error) {
         console.error(error);
-        alert("An error occurred while fetching posts.");
+        alert("An error occurred while deleting the post.");
+      }
     }
+  }
+
+  async function fetchPosts(accessToken) {
+    const response = await fetch(`${GET_POSTS}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+
+    return response.json();
+  }
+
+  function updatePostFeed(posts) {
+    const postFeedElement = document.getElementById("postFeed");
+    const loggedInUserEmail = localStorage.getItem("email");
+
+    if (postFeedElement) {
+      if (posts.length === 0) {
+        postFeedElement.innerHTML = "<p>No posts available.</p>";
+      } else {
+        postFeedElement.innerHTML = posts
+          .map(
+            (post) => `
+              <div class="card" id="post-${post.id}">
+              ${post.media ? `<img src="${post.media}" onerror="this.style.display='none'">` : ''}
+                <div class="card-content">
+                  <div class="author-info">
+                    <div class="profile-picture">
+                      <img src="${post.author.avatar || "/assets/user_avt.png"}" alt="">
+                    </div>
+                    <p class="card-user">${post.author.name}</p>
+                  </div>
+                  <h3 class="card-title">${post.title}</h3>
+                  <p class="card-description">${post.body || ""}</p>
+                  <a class="card-link readMoreBtn" id="readMoreLink${post.id}" href="#">Read More</a>
+                  ${
+                    post.author.email === loggedInUserEmail
+                      ? `<a class="card-link editBtn" data-postid="${post.id}" href="#">Edit</a>`
+                      : ""
+                  }
+                  ${
+                    post.author.email === loggedInUserEmail
+                      ? `<a class="card-link deleteBtn" data-postid="${post.id}" href="#">Delete</a>`
+                      : ""
+                  }
+                  <div class="post-id">Post ID: ${post.id}</div>
+                </div>
+              </div>
+            `
+          )
+          .join("");
+
+        const deleteButtons = document.querySelectorAll(".deleteBtn");
+
+        deleteButtons.forEach((button) => {
+          button.addEventListener("click", (e) => {
+            e.preventDefault();
+            const postId = e.target.dataset.postid;
+            deletePost(postId);
+          });
+        });
+
+        const editButtons = document.querySelectorAll(".editBtn");
+
+        editButtons.forEach((button) => {
+          button.addEventListener("click", (e) => {
+            e.preventDefault();
+            const postId = e.target.dataset.postid;
+            editPost(postId);
+          });
+        });
+      }
+    }
+  }
+
+  async function fetchAndDisplayPosts() {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (!accessToken) {
+      alert("Please log in to view posts.");
+      return;
+    }
+
+    try {
+      const posts = await fetchPosts(accessToken);
+      updatePostFeed(posts);
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while fetching posts.");
+    }
+  }
+
+  fetchAndDisplayPosts();
+
+  const filterMediaSelect = document.getElementById("filterByMedia");
+
+  filterMediaSelect.addEventListener("change", async function () {
+    const selectedValue = filterMediaSelect.value;
+    const accessToken = localStorage.getItem("accessToken");
+    let posts;
+
+    try {
+      const response = await fetch(`${GET_POSTS}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch posts");
+      }
+
+      posts = await response.json();
+
+      if (selectedValue === "withMedia") {
+        posts = posts.filter(post => post.media);
+      } else if (selectedValue === "withoutMedia") {
+        posts = posts.filter(post => !post.media);
+      }
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while fetching posts.");
+      return;
+    }
+
+    updatePostFeed(posts);
+  });
+
+  document.getElementById('searchButton').addEventListener('click', () => {
+    const searchTerm = document.getElementById('searchInput').value;
+    if (searchTerm.trim() !== '') {
+      searchPostsByTitle(searchTerm);
+    } else {
+      window.location.reload();
+    }
+  });
+
+  async function searchPostsByTitle(searchTerm) {
+    const accessToken = localStorage.getItem("accessToken");
+  
+    if (!accessToken) {
+      console.error("Access token not found in localStorage");
+      return;
+    }
+  
+    try {
+      const response = await fetch(`${GET_POSTS}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error("Failed to fetch posts");
+      }
+  
+      const posts = await response.json();
+      const filteredPosts = posts.filter(post => post.title.includes(searchTerm));
+  
+      updatePostFeed(filteredPosts);
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while searching for posts.");
+    }
+  }
+
 });
-
-
 
 
